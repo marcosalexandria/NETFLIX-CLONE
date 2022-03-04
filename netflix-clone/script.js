a735e4277d57c27d5666c95ed6e69fd66eb45169
Marcos Alexandria <93044961+marcosalexandria@users.noreply.github.com>
2022-03-04 19:57:52 -0300

    final

---
+++ netflix-clone/script.js
@@ -0,0 +1,18 @@
+
+// Para configurar o jquery e pegar funcionar o carrosel
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
